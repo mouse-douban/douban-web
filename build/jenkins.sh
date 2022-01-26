2ea26f62f88856d06433ecb57d3cbfe74556322e
iGxnon <1545766400@qq.com>
2022-01-26 20:13:12 +0800

    feat(live-load): add live-load support

--- build/jenkins.sh
+++ build/jenkins.sh
@@ -7,28 +7,28 @@ echo "Local dir $PWD"
 
 echo "Setting goproxy..."
 
-$GOROOT/bin/go env -w GOPROXY=https://goproxy.cn,direct
-$GOROOT/bin/go env -w GO111MODULE=on
-$GOROOT/bin/go mod tidy
+go env -w GOPROXY=https://goproxy.cn,direct
+go env -w GO111MODULE=on
+go mod tidy
 
 echo "Testing ..."
 
-$GOROOT/bin/go test -v test/*.go
+go test -v test/*.go
 
 echo "Building package ..."
 
 echo "Build linux..."
-$GOROOT/bin/go env -w GOARCH=amd64
-$GOROOT/bin/go env -w GOOS=linux
-$GOROOT/bin/go build -o target/linux/bin/gin ./cmd/main.go
+go env -w GOARCH=amd64
+go env -w GOOS=linux
+go build -o target/linux/bin/gin ./cmd/main.go
 
 echo "Build macos..."
-$GOROOT/bin/go env -w GOOS=darwin
-$GOROOT/bin/go build -o target/macos_amd64/bin/gin ./cmd/main.go
+go env -w GOOS=darwin
+go build -o target/macos_amd64/bin/gin ./cmd/main.go
 
-$GOROOT/bin/go env -w GOARCH=arm64
-$GOROOT/bin/go build -o target/macos_arm64/bin/gin ./cmd/main.go
+go env -w GOARCH=arm64
+go build -o target/macos_arm64/bin/gin ./cmd/main.go
 
 echo "Back to normal..."
-$GOROOT/bin/go env -w GOARCH=amd64
-$GOROOT/bin/go env -w GOOS=linux
+go env -w GOARCH=amd64
+go env -w GOOS=linux
